1b8ccc27b5f1b50a7d641170d0775c3bb55f585c
BauerJustin <justin.bauer@mail.utoronto.ca>
2022-09-18 16:57:23 -0700

    Save output as video

--- video.py
+++ video.py
@@ -1,8 +1,6 @@
 import cv2
 import torch
 
-frame_rate = 10
-
 model = torch.hub.load("../yolov5", 'custom', path="weights.pt", source='local')
 
 cam = cv2.VideoCapture("test.MOV")
@@ -10,6 +8,8 @@ timestamps = [cam.get(cv2.CAP_PROP_POS_MSEC)]
 
 currentframe = 0
 prev = 0
+frame_rate = int(cam.get(cv2.CAP_PROP_FPS))
+video = cv2.VideoWriter("test.avi", 0, frame_rate, (int(cam.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
 while(True):
 
@@ -23,13 +23,27 @@ while(True):
         curr = cam.get(cv2.CAP_PROP_POS_MSEC) / 1000
         timestamps.append(curr)
 
-        results = model(frame)    
-        
-        results.save()
-        print(f"Frame {currentframe}: {results.pandas().xyxy} Timestamp: {curr}")
+        detections = model(frame)
+        detections = detections.pandas().xyxy
+
+        for detection in detections:
+            if detection.empty:
+                break
+            tl = (int(detection['xmin'].values[0]), int(detection['ymin'].values[0]))
+            br =  (int(detection['xmax'].values[0]), int(detection['ymax'].values[0]))
+            label = detection['name'].values[0]
+            confidence = detection['confidence'].values[0]
+            text = '{}: {:.0f}%'.format(label, confidence * 100)
+            frame = cv2.rectangle(frame, tl, br, (0, 255, 0), 5)
+            frame = cv2.putText(frame, text, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+
+        video.write(frame)
+
+        print(f"Frame {currentframe}: {detections} Timestamp: {curr}")
         currentframe += 1
 
         prev = curr
 
+video.release()
 cam.release()
 cv2.destroyAllWindows()
