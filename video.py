feb7638c5e9159790d203c7c23d025eda04cda45
BauerJustin <justin.bauer@mail.utoronto.ca>
2022-09-18 15:21:17 -0700

    Add timestamp

--- video.py
+++ video.py
@@ -1,8 +1,12 @@
+from time import time
 import cv2
 import torch
 
 model = torch.hub.load("../yolov5", 'custom', path="weights.pt", source='local')
+
 cam = cv2.VideoCapture("test.MOV")
+timestamps = [cam.get(cv2.CAP_PROP_POS_MSEC)]
+
 currentframe = 0
   
 while(True):
@@ -10,10 +14,12 @@ while(True):
   
     if not ret:
         break
+    
+    timestamps.append(cam.get(cv2.CAP_PROP_POS_MSEC))
 
     results = model(frame)    
     results.save()
-    print(f"Frame {currentframe}: {results.pandas().xyxy}")
+    print(f"Frame {currentframe}: {results.pandas().xyxy} Timestamp: {timestamps[-1]}")
     currentframe += 1
 
 cam.release()
