76714496e21cbfa8e068d6b0f0a86bfcbe1f61b5
BauerJustin <justin.bauer@mail.utoronto.ca>
2022-09-18 15:36:48 -0700

    Add rate limiting to input video

--- video.py
+++ video.py
@@ -1,6 +1,7 @@
-from time import time
 import cv2
 import torch
+
+frame_rate = 10
 
 model = torch.hub.load("../yolov5", 'custom', path="weights.pt", source='local')
 
@@ -8,19 +9,27 @@ cam = cv2.VideoCapture("test.MOV")
 timestamps = [cam.get(cv2.CAP_PROP_POS_MSEC)]
 
 currentframe = 0
-  
+prev = 0
+
 while(True):
+
     ret, frame = cam.read()
-  
+    time_elapsed = (cam.get(cv2.CAP_PROP_POS_MSEC) / 1000) - prev
+
     if not ret:
         break
     
-    timestamps.append(cam.get(cv2.CAP_PROP_POS_MSEC))
+    if time_elapsed > 1.0 / frame_rate:
+        curr = cam.get(cv2.CAP_PROP_POS_MSEC) / 1000
+        timestamps.append(curr)
 
-    results = model(frame)    
-    results.save()
-    print(f"Frame {currentframe}: {results.pandas().xyxy} Timestamp: {timestamps[-1]}")
-    currentframe += 1
+        results = model(frame)    
+        
+        results.save()
+        print(f"Frame {currentframe}: {results.pandas().xyxy} Timestamp: {curr}")
+        currentframe += 1
+
+        prev = curr
 
 cam.release()
 cv2.destroyAllWindows()
